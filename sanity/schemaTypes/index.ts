1077630e3e24257dde676139301a509c49c2db32
noorjsdivs <noor.jsdivs@gmail.com>
2025-03-19 05:06:38 +0600

    sanity seed added

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,6 +1,24 @@
 import { type SchemaTypeDefinition } from "sanity";
 import { categoryType } from "./categoryType";
+import { productType } from "./productType";
+import { brandType } from "./brandType";
+import { addressType } from "./addressType";
+import { blockContentType } from "./blockContentType";
+import { authorType } from "./authorType";
+import { blogCategoryType } from "./blogCategoryType";
+import { blogType } from "./blogType";
+import { orderType } from "./orderType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType],
+  types: [
+    categoryType,
+    productType,
+    brandType,
+    addressType,
+    blockContentType,
+    authorType,
+    blogCategoryType,
+    blogType,
+    orderType,
+  ],
 };
